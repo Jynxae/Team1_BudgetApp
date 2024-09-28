1d68029c3fa1c89913de3bb944c09671482fd318
Reem Alkhalily <realkhalily@hotmail.com>
2024-09-28 00:51:57 -0500

    Initial Commit

---
+++ Team1_BudgetingApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Team1_BudgetingApp
+//
+//  Created by reem alkhalily on 9/28/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Team1_BudgetingApp/Team1_BudgetingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Team1_BudgetingAppApp.swift
+//  Team1_BudgetingApp
+//
+//  Created by reem alkhalily on 9/28/24.
+//
+
+import SwiftUI
+
+@main
+struct Team1_BudgetingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
