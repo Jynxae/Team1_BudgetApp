32cbaa5a30dec64f7461d30c2a8cea160cba811e
sue <suchanut3134@gmail.com>
2024-10-20 17:40:20 -0500

    added in tabview icon and financeView to contntView

--- Team1_BudgetingApp/ContentView.swift
+++ Team1_BudgetingApp/ContentView.swift
@@ -9,9 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var selectedTab: Int = 0
+    
+    // adjusts background color for tabview
+    init() {
+        UITabBar.appearance().backgroundColor = UIColor(named: "primaryLightPink") // Use your custom color
+        UITabBar.appearance().barTintColor = UIColor(named: "primaryLightPink") // Ensures compatibility with dark mode
+    }
+
+    
     var body: some View {
         VStack {
             TabView(selection: $selectedTab) {
+                MyFinancesView().tabItem {
+                    Image(systemName: "house.fill")
+                    Text("Home")
+                }
+                .tag(2)
+                
                 EditBudgetView().tabItem {
                     if selectedTab == 1 {
                         Image("piggybank-fill") // Use "filled" version from assets if available
