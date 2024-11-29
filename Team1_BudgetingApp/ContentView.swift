36e9e804eae23b3aec7f5de54a31505a3a9934cc
Reem Alkhalily <realkhalily@hotmail.com>
2024-11-28 19:59:54 -0600

    Added Firebase as Database

--- Team1_BudgetingApp/ContentView.swift
+++ Team1_BudgetingApp/ContentView.swift
@@ -1,8 +1,21 @@
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 struct ContentView: View {
     @State private var selectedTab: Int = 0
     @State private var isSignedIn: Bool = false
+    
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     
     var body: some View {
         VStack {
